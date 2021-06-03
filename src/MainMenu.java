896eb5b1df022721989724dbff993c06256391a1
Aditya Bagga <adityabagga05@gmail.com>
2021-06-02 22:54:50 -0700

    added highscore text

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -16,24 +17,39 @@ public class MainMenu extends BorderPane{
 	Button playGame;
 	Button instructions;
 	Application game;
+	int highScore = 0;
 	
 	public MainMenu(Application game) {
 		this.game = game;
 		HBox title = new HBox();
 		title.setPadding(new Insets(20, 0, 0, 0));
+		
+		HBox score = new HBox();
+		score.setPadding(new Insets(5, 0, 0, 0));
+		
+		VBox titleV = new VBox();
         
         Text titleText = new Text("Aditya's Breakout!");
         titleText.setFont(Font.font(null, 30));
         titleText.setFill(Color.BLACK);
         DropShadow dropShadow = new DropShadow(5, 5, 5, Color.GRAY);
         titleText.setEffect(dropShadow);
-
         
+        Text highScore = new Text("High score: ");
+        highScore.setFont(Font.font(null, 20));
+        highScore.setFill(Color.BLACK);
+        DropShadow dropShadow1 = new DropShadow(5, 5, 5, Color.GRAY);
+        highScore.setEffect(dropShadow1);
+  
         title.getChildren().addAll(titleText);
         title.setAlignment(Pos.CENTER);
         
-		
-		this.setTop(title); 
+        score.getChildren().addAll(highScore);
+        score.setAlignment(Pos.CENTER);
+        
+        titleV.getChildren().addAll(title, score);
+        
+		this.setTop(titleV);
 		
 		HBox buttons = new HBox();
 		buttons.setSpacing(50);
